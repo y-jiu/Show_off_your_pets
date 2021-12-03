ea74c276e966761a1c5f612439e4c0bb580249d2
y-jiu <chldudwn99@gmail.com>
2021-12-03 18:20:12 +0900

    Add save button to upload modal

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import ImageUploader from "react-images-upload";
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const Upload = () =>{
 
@@ -13,26 +14,31 @@ const Upload = () =>{
 
   return(
     <div>
-      <ImageUploader
-          withIcon={true}
-          buttonText="Choose images"
-          onChange={onDrop}
-          imgExtension={[".jpg", ".gif", ".png", ".gif"]}
-          maxFileSize={5242880}
-        />
-      <TextField
-          id="outlined-textarea"
-          label="Text"
-          variant="filled"
-          InputLabelProps={{
-            style: { color: '#fff'}, 
-        }}
-        InputProps={{
-          style: { color: '#fff'}
-        }}
-        style={{ width: "100%"}}
-          multiline
-        />
+      <div>
+        <ImageUploader
+            withIcon={true}
+            buttonText="Choose images"
+            onChange={onDrop}
+            imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+            maxFileSize={5242880}
+          />
+        <TextField
+            id="outlined-textarea"
+            label="Text"
+            variant="filled"
+            InputLabelProps={{
+              style: { color: '#fff'}, 
+          }}
+          InputProps={{
+            style: { color: '#fff'}
+          }}
+          style={{ width: "100%"}}
+            multiline
+          />
+        </div>
+        <div style = {{marginTop: "50px"}}>
+          <Button variant="contained" style = {{backgroundColor: "grey", float: "right"}}>Save</Button>
+        </div>
       </div>
   )
 }
